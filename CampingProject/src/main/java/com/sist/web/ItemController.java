0bcf29082724ca2771bbe04667863a74329fe415
SIST-112 <SIST-112@DESKTOP-MAT3KCP>
2025-05-09 09:36:33 +0900

    박현수 : 상품 상세페이지 작업중(25/04/29)

--- CampingProject/src/main/java/com/sist/web/ItemController.java
+++ CampingProject/src/main/java/com/sist/web/ItemController.java
@@ -1,18 +1,28 @@
 package com.sist.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.sist.service.*;
+import com.sist.vo.*;
+
 @Controller
 public class ItemController {
+	
+	@Autowired
+	private ItemService service;
+	
 	@GetMapping("item/list.do")
 	public String main(Model model) {
 		model.addAttribute("main_jsp","../item/list.jsp");
 		return "main/main";
 	}
 	@GetMapping("item/detail.do")
-	public String item_detail(Model model) {
+	public String item_detail(int ino,Model model) {
+		ItemVO vo=service.itemDetailData(ino);
+	   	model.addAttribute("vo", vo);
 		model.addAttribute("main_jsp","../item/detail.jsp");
 		return "main/main";
 	}
